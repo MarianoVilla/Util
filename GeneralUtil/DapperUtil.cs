403b375084c6cbb4886b9a0c39144a2157344d33
Mariano Villa <Mariano.Villa@outlook.com.ar>
2019-10-04 11:44:11 -0300

    Interval class and DapperUtil.

---
+++ GeneralUtil/DapperUtil.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeneralUtil
+{
+
+    public class DapperWrapper
+    {
+        public static IEnumerable<T> MapEntity<T>(T Entity, dynamic DapperResult) where T : new()
+        {
+            List<T> Output = new List<T>();
+            foreach (var row in DapperResult)
+            {
+                T Ent = new T();
+                var PropsOfT = Entity.GetType().GetProperties();
+                foreach (var p in PropsOfT)
+                {
+                    var data = (IDictionary<string, object>)row;
+                    var DataInRow = data[p.Name];
+                    if (DataInRow == null)
+                        continue;
+                    p.SetValue(Ent, DataInRow, null);
+                }
+                Output.Add(Ent);
+            }
+            return Output;
+        }
+    }
+}
---
+++ GeneralUtil/TestUtil.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace GeneralUtil
+{
+    public class ParamsComparer : Comparer<SqlParameter>
+    {
+        public override int Compare(SqlParameter x, SqlParameter y)
+        {
+            if (x.Value == null || y.Value == null)
+                return 0;
+            var NameCompare = x.ParameterName.CompareTo(y.ParameterName);
+            if (NameCompare == 0 && x.Value.ToString() == y.Value.ToString())
+                return 0;
+            return -1;
+        }
+    }
+    public class DynamicParams : DynamicParameters
+    {
+        public override bool Equals(object obj)
+        {
+            return CompareStrict(this, (DynamicParameters)obj);
+        }
+        public override int GetHashCode()
+        {
+            return this.ParameterNames.GetHashCode();
+        }
+        bool CompareStrict(DynamicParameters Me, DynamicParameters TheOther)
+        {
+            return Me.ParameterNames.All(xn =>
+            {
+                var ParamInX = Me.Get<dynamic>(xn);
+                var ParamInY = TheOther.Get<dynamic>(xn);
+                if (ParamInX == null)
+                {
+                    return (TheOther.Get<dynamic>(xn) == null);
+                }
+                return ParamInX.Equals(ParamInY);
+            });
+        }
+    }
+}

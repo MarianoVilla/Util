0c70deab9c91cbe78a7f8a7beed24adc5ec41e3f
Mariano Villa <Mariano.Villa@outlook.com.ar>
2019-10-04 18:06:38 -0300

    .

--- GeneralUtil/DapperUtil.cs
+++ GeneralUtil/DapperUtil.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
 
 namespace GeneralUtil
 {
 
     public class DapperWrapper
     {
+        string ConnectionString;
+        public DapperWrapper(string ConnectionString)
+        {
+            this.ConnectionString = ConnectionString;
+        }
         public static IEnumerable<T> MapEntity<T>(T Entity, dynamic DapperResult) where T : new()
         {
             List<T> Output = new List<T>();
@@ -26,6 +33,63 @@ namespace GeneralUtil
                 Output.Add(Ent);
             }
             return Output;
+        }
+
+        public IEnumerable<dynamic> Query(string QueryString)
+        {
+            using (IDbConnection db = new SqlConnection(ConnectionString))
+            {
+                return db.Query(QueryString);
+            }
+        }
+        public IEnumerable<dynamic> Query(string QueryString, DynamicParameters Parameters)
+        {
+            using (IDbConnection db = new SqlConnection(ConnectionString))
+            {
+                return db.Query(QueryString, Parameters);
+            }
+        }
+        public IEnumerable<dynamic> Query(string QueryString, object Parameters)
+        {
+            using (IDbConnection db = new SqlConnection(ConnectionString))
+            {
+                return db.Query(QueryString, Parameters);
+            }
+        }
+        public int ExecuteScalar(string QueryString)
+        {
+            using (IDbConnection db = new SqlConnection(ConnectionString))
+            {
+                return db.ExecuteScalar<int>(QueryString);
+            }
+        }
+        public int ExecuteScalar(string QueryString, DynamicParameters Parameters)
+        {
+            using (IDbConnection db = new SqlConnection(ConnectionString))
+            {
+                return db.ExecuteScalar<int>(QueryString, Parameters);
+            }
+        }
+        public int ExecuteScalar(string QueryString, object Parameters)
+        {
+            using (IDbConnection db = new SqlConnection(ConnectionString))
+            {
+                return db.ExecuteScalar<int>(QueryString, Parameters);
+            }
+        }
+        public int Execute(string QueryString, DynamicParameters Parameters)
+        {
+            using (IDbConnection db = new SqlConnection(ConnectionString))
+            {
+                return db.Execute(QueryString, Parameters);
+            }
+        }
+        public int Execute(string QueryString, object Parameters)
+        {
+            using (IDbConnection db = new SqlConnection(ConnectionString))
+            {
+                return db.Execute(QueryString, Parameters);
+            }
         }
     }
 }
--- GeneralUtil/DateUtil.cs
+++ GeneralUtil/DateUtil.cs
@@ -31,6 +31,18 @@ namespace Dager
             long unixTimeStampInTicks = (long)(unixTime * TimeSpan.TicksPerSecond);
             return new DateTime(unixStart.Ticks + unixTimeStampInTicks, DateTimeKind.Utc);
         }
+        public static DateTime PreviousWorkDay(DateTime date)
+        {
+            do
+            {
+                date = date.AddDays(-1);
+            } while (IsWeekend(date));
+            return date;
+        }
+        public static bool IsWeekend(DateTime date)
+        {
+            return date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday;
+        }
 
     }
 }
